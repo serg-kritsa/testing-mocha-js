ff37bec7df6dd244057f13e377a2fe10694c9f09
Serg <serg@example.com>
2021-12-02 00:37:35 +0300

    simple passed test

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,9 @@
+const assert = require('assert');
 
 describe('tests group description', () => {
   context('tests sub-group description', () => {
     it('should be 1st test', () => {
+      assert.equal(1,1);  
     });
   });
 });
